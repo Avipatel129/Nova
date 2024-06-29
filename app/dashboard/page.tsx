a84cc564edfc248e7662ed1e6ce0c70a1a4b82db
Avinash Patel <abinashpatel.ap@gmail.com>
2024-06-29 19:58:55 +0530

    feat: added gemini api

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,25 @@
-import React from "react";
+"use client";
+import React, { use, useEffect } from "react";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
 export default function Dashboard() {
+  useEffect(() => {
+    // Access your API key as an environment variable (see "Set up your API key" above)
+    const genAI = new GoogleGenerativeAI(
+      "AIzaSyAlw1WnfkTsaxBCJU53wbXHg3JhRq4M7M0"
+    );
+
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+    async function run() {
+      const prompt = "Who is the precident of india?";
+
+      const result = await model.generateContent(prompt);
+      const response = await result.response;
+      const text = response.text();
+      console.log(text);
+    }
+
+    run();
+  }, []);
   return <div>Hello from Dashboard </div>;
 }
