c17b0365a8000764c3f7327292b4029b9d8c13b1
Avinash Patel <abinashpatel.ap@gmail.com>
2024-06-29 19:15:52 +0530

    build: Landing Page

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -1,11 +1,23 @@
 import React from "react";
 import { ThemeToggle } from "./ThemeToggle";
+import { Button } from "./ui/button";
+import { ArrowRight } from "lucide-react";
+import Link from "next/link";
 
 export default function Landing() {
-    return (
-        <div>
-            <p>Hello World This is Suman</p>
-            <ThemeToggle />
-        </div>
-    );
+  return (
+    <div id="landing-page-cotainer" className="center flex-col">
+      <ThemeToggle />
+      <h1 className="text-4xl font-semibold">Nova</h1>
+      <p className="text-lg text-center">
+        Brightening your day with smart conversations
+      </p>
+      <Link href={"/dashboard"}>
+        <Button className="center gap-2">
+          <p>Get started</p>
+          <ArrowRight size={16} />
+        </Button>
+      </Link>
+    </div>
+  );
 }
