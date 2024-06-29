07b31872b93d755562d60cd0ec111c03a47b7018
Avinash Patel <abinashpatel.ap@gmail.com>
2024-06-29 13:21:29 +0530

    starter template

---
+++ components/Landing.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { ThemeToggle } from "./ThemeToggle";
+
+export default function Landing() {
+  return (
+    <div>
+      HelloWorld
+      <ThemeToggle />
+    </div>
+  );
+}
