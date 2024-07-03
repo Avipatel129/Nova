cd6a834d0a312f70aa9ab23c6cd5404f157c4c5d
Avinash Patel <abinashpatel.ap@gmail.com>
2024-07-03 23:05:07 +0530

    feat: added user prompt

---
+++ ai.ts
@@ -0,0 +1,7 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+const GOOGLE_API_KEY: any = process.env.NEXT_PUBLIC_GOOLGE_API_KEY;
+
+const genAI = new GoogleGenerativeAI(GOOGLE_API_KEY);
+
+export const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,41 +1,40 @@
 "use client";
-import React, { use, useEffect } from "react";
-import { GoogleGenerativeAI } from "@google/generative-ai";
-import { addDoc, collection } from "firebase/firestore";
-import { db } from "@/firebase";
+import React, { useEffect, useRef, useState } from "react";
+import { model } from "@/ai";
 
 export default function Dashboard() {
-  const GOOGLE_API_KEY: any = process.env.NEXT_PUBLIC_GOOLGE_API_KEY;
-  useEffect(() => {
-    const genAI = new GoogleGenerativeAI(GOOGLE_API_KEY);
+  const [response, setResponse] = useState<string | null>(null);
+  const [prompt, setPrompt] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<boolean>(false);
+  const promptRef = useRef<HTMLInputElement | null>(null);
 
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-    async function run() {
-      const prompt = "Who is the precident of india?";
-
-      const result = await model.generateContent(prompt);
-      const response = await result.response;
-      const text = response.text();
-      console.log(text);
-    }
-
-    // run();
-  }, []);
-
-  const handleSubmit = async (e: any) => {
+  const generateResponse = async () => {
+    setLoading(true);
     try {
-      await addDoc(collection(db, "user"), {
-        name: "suman",
-        lname: "gurung",
-      });
+      if (promptRef.current) {
+        const result = await model.generateContent(promptRef.current.value);
+        const res = await result.response;
+        const text = res.text();
+        setResponse(text);
+      }
     } catch (err) {
-      alert(err);
+      setError(true);
+    } finally {
+      setLoading(false);
     }
   };
+  useEffect(() => {
+    if (prompt != null) generateResponse();
+  }, [prompt]);
 
+  if (loading) return <p>Loading</p>;
+  if (error) return <p>Error</p>;
   return (
     <div>
-      <button>save</button>
+      {response}
+      <input type="text" ref={promptRef} />
+      <button onClick={generateResponse}>Generate</button>
     </div>
   );
 }
